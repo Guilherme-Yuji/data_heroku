6f57c1a02138f8c7bc70e123681259920493c0bb
Guilherme-Yuji <78933005+Guilherme-Yuji@users.noreply.github.com>
2021-06-02 19:46:32 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -27,27 +27,54 @@ def build_model(data):
 	st.text(numerical_attributes)
 
 
-	st.markdown('**1.3- Basic Statistics**')
-	st.text(data.describe())
+	st.markdown('**1.3- Duplicated values**')
+	st.text(data.duplicated().sum())
 
 	st.markdown('**1.4- Missing values**')
 	st.text(data.isnull().sum())
+
+	st.markdown('**1.5- Unique values in the Categorical Variables**')
+	for col_name in data.columns:
+		 if data[col_name].dtypes == 'object':
+		 	unique_cat = len(data[col_name].unique())
+ 			st.text("Feature '{col_name}' has {unique_cat} unique categories".format(col_name=col_name, unique_cat=unique_cat))
 	
 
 	st.subheader('2- Exploratory Data Analysis (EDA)')
 	hue = data.columns[-1]
 
-	st.markdown('**2.1- Numerical Variables**')
+	st.markdown('**2.1- Descriptive Statistics**')
+	st.text(data.describe())
+
+	st.markdown('**2.2- Outlier detectetion by Boxplot**')
+	for a in numerical_attributes:
+		st.text(a)
+		fig = plt.figure(figsize = (20,10))
+		sns.boxplot(data[a])
+		st.pyplot(fig)
+
+
+	st.markdown('**2.3- Target Variable plot**')
+	st.text("Target variable:" + hue)
+	fig = plt.figure(figsize = (20,10))
+	ax = sns.countplot(data[hue])
+	for p in ax.patches:
+		height = p.get_height()
+		ax.text(x = p.get_x()+(p.get_width()/2), y  = height*1.01, s = '{:.0f}'.format(height), ha = 'center')
+	st.pyplot(fig)
+
+
+	st.markdown('**2.4- Numerical Variables**')
 	#fig = plt.figure(figsize = (5,5))
 	#sns.pairplot(data, hue = hue)
 	#st.pyplot(fig)
 
-	st.text('Correlation:')
+	st.markdown('***2.4.1- Correlation***')
 	fig = plt.figure(figsize = (20,10))
 	sns.heatmap(data.corr(), cmap = 'Blues', annot = True)
 	st.pyplot(fig)
 
-	st.text('Distribution:')
+	st.markdown('***2.4.2- Distributions***')
 	for a in numerical_attributes:
 		st.text(a)
 		fig = plt.figure(figsize = (20,10))
@@ -56,14 +83,8 @@ def build_model(data):
 
 	
 
-	st.markdown('**2.2- Categorical Variables**')
-	st.text(hue)
-	fig = plt.figure(figsize = (20,10))
-	ax = sns.countplot(data[hue])
-	for p in ax.patches:
-			height = p.get_height()
-			ax.text(x = p.get_x()+(p.get_width()/2), y  = height*1.01, s = '{:.0f}'.format(height), ha = 'center')
-	st.pyplot(fig)
+	st.markdown('**2.5- Categorical Variables**')
+
 
 	for a in categorical_attributes:
 		if a == hue:
@@ -81,7 +102,10 @@ def build_model(data):
 
 st.write("""
 # Data Science App
-In this implementation, you can the do a full analysis of your dataset anf got a DS solution!
+In this implementation, you can to the EDA of our dataset to speed-up our analysis! \n
+To use this app, First you need to import your dateset in the sidebar on the left and then just wait for the results. \n
+NOTES: \n
+The target variable it will be considered to be the last column of the dataset.
 """)
 
 
@@ -103,8 +127,10 @@ st.subheader('1- Dataset')
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
-    st.markdown('**1.1. Dataset:**')
+    st.markdown('**1.1. Dataset info:**')
+    st.write('First 5 rows of the dataset:')
     st.write(df.head())
+    st.write('Last 5 rows of the dataset:')
     st.write(df.tail())
     build_model(df)
 else:
